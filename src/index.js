546953374d111fd6755ceed146901af544350331
Taka <necojackarc@users.noreply.github.com>
2020-09-01 17:16:55 +0100

    Don't run the command when tests are running (#24)

--- src/index.js
+++ src/index.js
@@ -60,8 +60,11 @@ async function run() {
   await github.assign_reviewers(reviewers);
 }
 
-run().catch((error) => core.setFailed(error));
-
 module.exports = {
   run,
 };
+
+// Run the action if it's not running in an automated testing environment
+if (process.env.NODE_ENV !== 'automated-testing') {
+  run().catch((error) => core.setFailed(error));
+}
